c29bce02d0514e93701e4c0146b2f7001dbd74ef
Arelis Orozco Picado <aorozco@gticr.com>
2020-09-19 14:19:33 -0600

    Ajustes sobre login del sistema

--- SistemaSeguros/Scripts/CuentaUsuario/Login.js
+++ SistemaSeguros/Scripts/CuentaUsuario/Login.js
@@ -12,78 +12,90 @@ var Administracion = {
             return true;
         }
     }
- 
-}
 
-$(document).ready(function () {
-    $(btnIniciar).on('click', function () {
-        if ($(credencial).val() != "" && $(correo).val() != "") {
-            if (Administracion.validar_email($(correo).val())) {
-                //Envío al API 
-                var usuario = { correo: $(correo).val(), credencial: $(credencial).val() };
- 
-                $.ajax({
-                    type: "POST",
-                    data: JSON.stringify(usuario),
-                    url: UrlAPI + "/CuentaUsuario/InicioSesion"
-                }).done(function (data) {
-                    if (data != "") {
-                        //Usuario existe
-                        
-                        $.ajax({
-                            url: "/Home/CrearCookie",
-                            type: "POST",
-                            data: {
-                                'Usuario': data
-                            },
-                            datatype: "json",
-                            success: function (data) {
-                                if (data.datosCliente) {
-                                    window.location.href = "/Home/Index";
-                                }
+}
+function IniciarSesion() {
+    if ($(credencial).val() != "" && $(correo).val() != "") {
+        if (Administracion.validar_email($(correo).val())) {
+            //Envío al API 
+            var usuario = { correo: $(correo).val(), credencial: $(credencial).val() };
+
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify(usuario),
+                url: UrlAPI + "/CuentaUsuario/InicioSesion"
+            }).done(function (data) {
+                if (data != "") {
+                    //Usuario existe
+
+                    $.ajax({
+                        url: "/Home/CrearCookie",
+                        type: "POST",
+                        data: {
+                            'Usuario': data
+                        },
+                        datatype: "json",
+                        success: function (data) {
+                            if (data.datosCliente) {
+                                window.location.href = "/Home/Index";
                             }
-                        });
-                    } else {
-                        swal({
-                            title: 'Sistema de Seguros',
-                            text: "Los datos ingresados, no están registrados en el Sistema.!",
-                            type: 'error',
-                            showCancelButton: true,
-                            confirmButtonColor: '#3085d6',
-                            cancelButtonColor: '#d33'
-                        })
-                    }
-                }).fail(function (jqXHR, textStatus, errorThrown) {
+                        }
+                    });
+                } else {
                     swal({
                         title: 'Sistema de Seguros',
-                        text: "No se pudo iniciar sesión",
+                        text: "Los datos ingresados, no están registrados en el Sistema.!",
                         type: 'error',
                         showCancelButton: true,
                         confirmButtonColor: '#3085d6',
                         cancelButtonColor: '#d33'
                     })
-                 });
-
-            } else {
+                }
+            }).fail(function (jqXHR, textStatus, errorThrown) {
                 swal({
                     title: 'Sistema de Seguros',
-                    text: "El formato del correo no es el correcto!",
+                    text: "No se pudo iniciar sesión",
                     type: 'error',
                     showCancelButton: true,
                     confirmButtonColor: '#3085d6',
                     cancelButtonColor: '#d33'
                 })
-            }
+            });
+
         } else {
-            //faltan datos
             swal({
                 title: 'Sistema de Seguros',
-                text: "Debe ingresar un correo y una credencial, para poder iniciar",
+                text: "El formato del correo no es el correcto!",
                 type: 'error',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33'
             })
         }
+    } else {
+        //faltan datos
+        swal({
+            title: 'Sistema de Seguros',
+            text: "Debe ingresar un correo y una credencial, para poder iniciar",
+            type: 'error',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33'
+        })
+    }
+}
+$(document).ready(function () {
+    $(btnIniciar).on('click', function () {
+        IniciarSesion();
     });
+});
+
+$(document).on("keypress", "input", function (e) {
+
+    if (e.which == 13) {
+
+        IniciarSesion();
+
+    }
+
 });
