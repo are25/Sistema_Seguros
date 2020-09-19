126dec86b358378121ac6660a44cd954e7ed7e49
Arelis Orozco Picado <aorozco@gticr.com>
2020-09-19 00:17:38 -0600

    Creación de proyecto para pruebas unitarias.

---
+++ SistemaSeguros.Test/AccesoInformacion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaSeguros.API.Models;
+using SistemaSeguros.API.Controllers;
+using System.Web.Http.Results;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SistemaSeguros.Test
+{
+    [TestClass]
+    public class AccesoInformacion
+    {
+        [TestMethod]
+        public void AgregarCliente()
+        {
+            // Arrange
+            var controller = new ClientesController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
+
+            Clientes cli = new Clientes()
+            {
+                IdentificacionCliente = "304670958",
+                NombreCliente = "Arelis Orozco",
+                CorreoCliente = "arelisd.25@gmail.com",
+                TelefonoContacto = "71510015"
+            };
+
+            // Act
+            var response = controller.RegistroCliente();
+
+            // Assert
+            Clientes product;
+            Assert.IsTrue(response.TryGetContentValue<Clientes>(out product));
+            Assert.AreEqual(10, product.IdentificacionCliente);
+        }
+    }
+}
---
+++ SistemaSeguros.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SistemaSeguros.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SistemaSeguros.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a60f9dcb-b942-4b0d-9c35-616610999944")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
